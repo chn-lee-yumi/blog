729c70d6cd604c464483058305ce780908c730f9
liyumin <chn.lee.yumi@gmail.com>
2026-06-11 00:21:35 +1000

    add 202601Travel (day3-6)

--- compress_videos.sh
+++ compress_videos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # 下面是示例，注意帧率要先用ffprobe看看原视频，然后再设置
-input_file="content/post/2026/ANU-Quantum-Science-Open-Day/IMG_1534.MOV"
-output_file="content/post/2026/ANU-Quantum-Science-Open-Day/IMG_1534_2.webm"
-ffmpeg -i "$input_file" -c:v libvpx-vp9 -b:v 1M -r 29.57 -c:a libopus "$output_file"
+input_file="content/post/2026/202601Travel/IMG_2156.mov"
+output_file="content/post/2026/202601Travel/IMG_2156.webm"
+ffmpeg -y -i "$input_file" -c:v libvpx-vp9 -b:v 1M -r 29.67 -c:a libopus "$output_file"
